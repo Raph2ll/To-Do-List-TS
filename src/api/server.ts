22457f97b058e80060948642cbc294071fabec33
raph2ll <raphael.ph2ll@gmail.com>
2022-12-04 19:20:14 -0300

    feat: add cors and router

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import router from '../router';
+import cors from 'cors';
 
 mongoose.connect('mongodb://localhost:27017/test')
   .then(() => {
-    const app = express();
     const PORT = process.env.PORT || 3000;
 
+    const app = express();
+    app.use(express.json());
+    app.use(cors());
 
+    app.use('/', router);
     app.listen(PORT, () => console.log(`server is running on localhost:${PORT}`));
   }).catch((error) => {
     console.log(error);
