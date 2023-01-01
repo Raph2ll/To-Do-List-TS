f81486a52d1a96cc4feb22bfe1685dcfad137d70
raph2ll <raphael.ph2ll@gmail.com>
2023-01-01 17:32:15 -0300

    refactor: api

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,17 +1,13 @@
-import express from 'express';
 import mongoose from 'mongoose';
-import router from '../router';
-import cors from 'cors';
+import app from './app';
 
-mongoose.connect('mongodb://localhost:27017/test')
+
+mongoose
+  .connect('mongodb://localhost:27017/test')
   .then(() => {
     const PORT = process.env.PORT || 3000;
 
-    const app = express();
-    app.use(express.json());
-    app.use(cors());
 
-    app.use('/', router);
     app.listen(PORT, () => console.log(`server is running on localhost:${PORT}`));
   }).catch((error) => {
     console.log(error);
