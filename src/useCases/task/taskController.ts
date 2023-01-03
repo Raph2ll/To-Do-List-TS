babe819bf4d3a620bab9d9334011793650704fca
raph2ll <raphael.ph2ll@gmail.com>
2023-01-03 16:03:11 -0300

    feat: schema validation

--- src/useCases/task/taskController.ts
+++ src/useCases/task/taskController.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from 'express';
 import StatusCodes from 'http-status-codes';
-import createTask from './taskService';
+import { createTask } from './taskService';
+import schema from '../../schemas/schemaTask';
 
 const create = async (req: Request, res: Response) => {
   try {
     const { description } = req.body;
-    const response = await createTask(description);
 
+    const { error } = schema.validate({ description });
+    if (error) return res.status(StatusCodes.BAD_REQUEST).json(error.message);
+
+    const response = await createTask(description);
     return res.status(StatusCodes.CREATED).json(response);
   } catch (error) {
     console.log(error);
