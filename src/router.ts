f74dfb4ea52fffa0acb5ccec2edb92ce09fe25a6
raph2ll <raphael.ph2ll@gmail.com>
2023-01-16 14:21:06 -0300

    feat: router new path

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,8 @@
 import express from 'express';
-import { create, list } from './useCases/task/taskController';
+import { create, list, findByDescription } from './useCases/task/taskController';
 const route = express.Router({ mergeParams: true });
 
 route.post('/task', create);
 route.get('/task', list);
-
+route.get('/task/:description', findByDescription);
 export default route;
