4b580efe6221559153bf7a7d240f8bd279735033
raph2ll <raphael.ph2ll@gmail.com>
2023-01-18 15:32:09 -0300

    refactor: model createdAt type

--- src/model/Task.ts
+++ src/model/Task.ts
@@ -4,7 +4,7 @@ import { format } from 'date-fns';
 const timestamp = format(new Date(), 'dd-MM-yyy HH:mm:ss');
 
 export const Task = model('Task', new Schema({
-  createdAt: { type: Date, default: timestamp },
+  createdAt: { type: String, default: timestamp },
   updatedAt: { type: Date },
   description: { type: String, required: true },
   completed: { type: Boolean, default: false },
